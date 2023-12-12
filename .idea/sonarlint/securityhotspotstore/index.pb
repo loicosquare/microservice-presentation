33a737e5e4c2f77827dc2fdb1152b81a54f60a38
Loic Sanou <loicsan07@gmail.com>
2023-12-12 14:02:26 +0100

    Adding multiples routes on APIGATEWAY

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -89,3 +89,5 @@ Gsrc/main/java/com/tech/user/service/external/services/HotelService.java,e\9\e9
 -src/main/java/com/quiz/entities/Question.java,a\8\a8c2ced73ed44ffe0d3e9f19b7a37b120371ac54
 |
 Lsrc/main/java/com/tech/hotel/service/HotelService/services/HotelService.java,a\0\a02794201a1a8ecf39550f45bf2b248bb1470a2f
+Å
+Qsrc/main/java/com/tech/hotel/service/HotelService/controller/StaffController.java,9\b\9b9fc6252f2172bcbb376eb9be416eadba8a8519
--- HotelService/src/main/java/com/tech/hotel/service/HotelService/controller/StaffController.java
+++ HotelService/src/main/java/com/tech/hotel/service/HotelService/controller/StaffController.java
@@ -1,2 +1,21 @@
-package com.tech.hotel.service.HotelService.controller;public class StaffController {
+package com.tech.hotel.service.HotelService.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> getStaffs() {
+        List<String> list = Arrays.asList("Eugène", "Cissoko", "Loic", "Armel");
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
 }
