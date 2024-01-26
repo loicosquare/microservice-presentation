4c065ecb8b80a7ffaf193ec24f2292b1f07544f2
Loic Sanou <loicsan07@gmail.com>
2024-01-26 21:34:48 +0100

    Updating Frontend menu

--- frontOffice/src/app/pages/sidebar-menu/sidebar-menu.component.ts
+++ frontOffice/src/app/pages/sidebar-menu/sidebar-menu.component.ts
@@ -12,6 +12,11 @@ export class SidebarMenuComponent {
     { routerLink: 'enterprises', routerLinkActive: 'active', iconClass: 'fas fa-building', text: 'Enterprises' },
     { routerLink: 'games', routerLinkActive: 'active', iconClass: 'fas fa-gamepad', text: 'Games' },
     { routerLink: 'categories', routerLinkActive: 'active', iconClass: 'fas fa-list-alt', text: 'Categories' },
-    { routerLink: 'users', routerLinkActive: 'active', iconClass: 'fas fa-users', text: 'Users' }
+    { routerLink: 'users', routerLinkActive: 'active', iconClass: 'fas fa-users', text: 'Users' },
+    { routerLink: 'payments', routerLinkActive: 'active', iconClass: 'fas fa-dollar-sign', text: 'Payments' },
+    { routerLink: 'orders', routerLinkActive: 'active', iconClass: 'fas fa-shopping-cart', text: 'Orders' },
+    { routerLink: 'deliveries', routerLinkActive: 'active', iconClass: 'fas fa-truck', text: 'Deliveries' },
+    { routerLink: 'quizzes', routerLinkActive: 'active', iconClass: 'fas fa-question', text: 'Quizzes' },
+    { routerLink: 'questions', routerLinkActive: 'active', iconClass: 'fas fa-question-circle', text: 'Questions' }
   ];
 }
