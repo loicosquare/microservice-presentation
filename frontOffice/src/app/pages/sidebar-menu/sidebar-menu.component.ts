2b0a440905b89bebfc0088f66b459183eb00fe4f
Loic Sanou <loicsan07@gmail.com>
2024-02-08 11:49:44 +0100

    update Front back.

--- frontOffice/src/app/pages/sidebar-menu/sidebar-menu.component.ts
+++ frontOffice/src/app/pages/sidebar-menu/sidebar-menu.component.ts
@@ -8,7 +8,7 @@ import {MenuItem} from "../../interface/menu-item";
 })
 export class SidebarMenuComponent {
   menuItems: MenuItem[] = [
-    { routerLink: 'y', routerLinkActive: 'active', iconClass: 'fas fa-bars', text: 'Menu' },
+    { routerLink: '', routerLinkActive: 'active', iconClass: 'fas fa-bars', text: 'Menu' },
     { routerLink: 'enterprises', routerLinkActive: 'active', iconClass: 'fas fa-building', text: 'Enterprises' },
     { routerLink: 'games', routerLinkActive: 'active', iconClass: 'fas fa-gamepad', text: 'Games' },
     { routerLink: 'categories', routerLinkActive: 'active', iconClass: 'fas fa-list-alt', text: 'Categories' },
